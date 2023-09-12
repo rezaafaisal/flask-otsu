0354e50d484042537a16569f10f59d12fcc4bfbd
rezaafaisal <rezha.faizal567@gmail.com>
2023-09-13 00:11:52 +0800

    finish

--- bot.py
+++
@@ -1,4 +0,0 @@
-import requests
-
-response = requests.get('https://api.telegram.org/bot6391112787:AAGs2P9-7ELdc634teCC7e7wKysqIqa7A7g/setWebhook?url=https://46b9-120-188-87-196.ngrok-free.app/')
-print(response)
